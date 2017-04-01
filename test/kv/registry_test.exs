d0147c6817bc89e30c46043f38144406c211b50f
Leonardo Tegon <ltegon93@gmail.com>
2017-04-01 11:46:48 -0300

    Add Supervisors

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,8 +1,8 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    {:ok, registry} = KV.Registry.start_link
+  setup context do
+    {:ok, registry} = KV.Registry.start_link(context.test)
     {:ok, registry: registry}
   end
 
@@ -16,10 +16,24 @@ defmodule KV.RegistryTest do
     assert KV.Bucket.get(bucket, "milk") == 1
   end
   
-  test "removes buckets on exit", %{registry: registry} do
+  test "removes bucket on exit", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    ref = Process.monitor(bucket)
+    Process.exit(bucket, :shutdown)
+
+    # Wait until the bucket is dead
+    assert_receive {:DOWN, ^ref, _, _, _}
+
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
